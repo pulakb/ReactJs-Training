e9bef840a8a92f1f89f2e62d56e40c1907fdaa68
Pulak <bhattacharyyapulak@gmail.com>
2015-09-02 18:50:26 +0530

    Photo Gallery example

--- PhotoGallery/scripts/photo.js
+++ PhotoGallery/scripts/photo.js
@@ -30,5 +30,33 @@ var Photo = React.createClass({
     }
 });
 
+var PhotoGallery = React.createClass({
+    render: function () {
+        var photos = this.props.photos.map(function (photo) {
+            return <Photo src={photo.url} caption={photo.caption} />
+        });
 
-React.render(<Photo src="http://photos-b.ak.instagram.com/hphotos-ak-xfa1/t51.2885-15/10853090_395670790609953_769522691_n.jpg" caption="Building" />, document.body);
+        return (
+            <div className='photo-gallery'>
+            {photos}
+            </div>
+        );
+    }
+});
+
+var data = [
+    {
+        url: 'http://photos-b.ak.instagram.com/hphotos-ak-xfa1/t51.2885-15/10853090_395670790609953_769522691_n.jpg',
+        caption: 'Hong Kong!'
+    },
+    {
+        url: 'http://photos-e.ak.instagram.com/hphotos-ak-xaf1/t51.2885-15/10852852_357115861137756_207463750_n.jpg',
+        caption: 'Cows'
+    },
+    {
+        url: 'http://photos-c.ak.instagram.com/hphotos-ak-xaf1/t51.2885-15/10832107_406647066158250_128485237_n.jpg',
+        caption: 'Scooters'
+    }
+];
+
+React.render(<PhotoGallery photos={data} />, document.body);
